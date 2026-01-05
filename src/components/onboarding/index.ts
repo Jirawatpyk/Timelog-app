0754252069666767b80b44afa80e9318c78b55be
Jirawat Pyk <jirawat.pyk@gmail.com>
2026-01-05 17:23:58 +0700

    docs: Add JSDoc to onboarding index.ts (L1 fix)

--- src/components/onboarding/index.ts
+++ src/components/onboarding/index.ts
@@ -1,2 +1,8 @@
+/**
+ * Onboarding Components
+ * Story 8.7: First-Time User Flow
+ *
+ * Components for the new user onboarding experience.
+ */
 export { WelcomeScreen } from './WelcomeScreen';
 export { FeatureCard } from './FeatureCard';
